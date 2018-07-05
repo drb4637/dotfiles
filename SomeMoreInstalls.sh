e91a2b005a82ecde54e8424a8f7d0020ff6c2a01
Daniel Bower <drb4637@gmail.com>
2018-07-05 17:50:52 -0400

    added some more git stuff to the files

--- SomeMoreInstalls.sh
+++ SomeMoreInstalls.sh
@@ -3,3 +3,6 @@
 sudo apt-get install nmap -y
 sudo apt-get install netcat -y
 
+#this is some git related things, should prolly go someplace else
+git config --global credential.helper cache 
+git config --global credential.helper 'cache --timeout=6000'
