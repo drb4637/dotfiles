dfd21fc64b39887d4642f7336086c29fe7b99bc5
Daniel Bower <drb4637@gmail.com>
2018-07-05 16:59:06 -0400

    Added a bit more to the inital setup file

--- otherInit.sh
+++ otherInit.sh
@@ -1,3 +1,8 @@
 #/bin/bash
 
-echo llalalalalaal
+echo Getting the initial system setup 
+echo Switching the capslock and escape keys
+setxkbmap -option caps:swapescape
+setxkbmap -option swapescape:caps
+
+sudo apt-get install vim -y
